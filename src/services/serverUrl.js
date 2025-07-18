4dcbf6e03e37fdcd39604f109a7e918456e649d0
savidhps <savidhps00@gmail.com>
2025-07-18 21:05:42 +0530

    added render link

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,1 +1,4 @@
-export const serverUrl='http://localhost:4000'
+// used when running localy
+// export const serverUrl='http://localhost:4000'
+
+export const serverUrl='https://docpen-backend.onrender.com'
